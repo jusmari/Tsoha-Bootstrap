48b75f7790578d5d01149932fc445e495f26ec26
Jussi Maristo <maot@cs.helsinki.fi>
2017-05-25 14:52:47 +0300

    create and drop tables

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,12 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+
+INSERT INTO Usr (name, password) VALUES ('testi', 'testi');
+INSERT INTO Usr (name, password) VALUES ('testi1', 'testi1');
+
+INSERT INTO Question (body, correctAnswer, possibleAnswers) VALUES ('testikysymys', 'kyllä', 'kyllä;ei');
+INSERT INTO Question (body, correctAnswer, possibleAnswers) VALUES ('toinenTestikysymys', 'kyllä', 'kyllä;ei');
+
+
+INSERT INTO Organization (name) VALUES ('Testisad');
+INSERT INTO Organization (name) VALUES ('asnkdkasndk');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+
+CREATE TABLE Usr(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Question(
+  id SERIAL PRIMARY KEY,
+  body varchar(140) NOT NULL,
+  correctAnswer varchar(140) NOT NULL,
+  possibleAnswers varchar(200) NOT NULL
+);
+
+CREATE TABLE Organization(
+  id SERIAL PRIMARY KEY,
+  name varchar(140) NOT NULL
+);
+
+CREATE TABLE Membership(
+  organization_id INTEGER REFERENCES Organization(id),
+  usr_id INTEGER REFERENCES Usr(id)
+);
+
+CREATE TABLE Answers(
+  usr_id INTEGER REFERENCES Usr(id),
+  question_id INTEGER REFERENCES Question(id)
+);
